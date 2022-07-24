8b24ec842a880c25021b280d99451f71b6f49180
wecansur <vlkob95@gmail.com>
2022-07-24 13:07:56 +0800

    feat: 13/20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let checkingStack = [];
+  for (i = 0; i < str.length; i++) {
+    for (k = 0; k < bracketsConfig.length; k++) {
+      if (str[i] === bracketsConfig[k][0]) {
+        checkingStack.push(str[i]);
+      } else if (str[i] === bracketsConfig[k][1]) {
+        checkingStack[checkingStack.length - 1] === bracketsConfig[k][0] ? checkingStack.pop() : false
+      }
+    }
+    
+  }
+  return (checkingStack.length === 0);
 }
