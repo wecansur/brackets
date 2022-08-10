0bff4daa62727ac64cff177c756c1269ce61561f
wecansur <vlkob95@gmail.com>
2022-08-10 17:13:09 +0800

    feat: change check to includes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
   let checkingStack = [];
+  let sameBracketcheckingStack = [];
   for (i = 0; i < str.length; i++) {
     for (k = 0; k < bracketsConfig.length; k++) {
       if (str[i] === bracketsConfig[k][0]) {
         checkingStack.push(str[i]);
       } else if (str[i] === bracketsConfig[k][1]) {
-        checkingStack[checkingStack.length - 1] === bracketsConfig[k][0] ? checkingStack.pop() : false
+        checkingStack.includes(bracketsConfig[k][0]) ? checkingStack.pop() : false
       }
     }
     
